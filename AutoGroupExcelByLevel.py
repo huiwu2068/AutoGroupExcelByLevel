94f83d8b605a978f3ddaa33296fe1b12669e70d7
huiwu2068 <huiwu2068@sina.com>
2021-02-07 14:58:10 +0800

    Update AutoGroupExcelByLevel.py

--- AutoGroupExcelByLevel.py
+++ AutoGroupExcelByLevel.py
@@ -1,3 +1,6 @@
+#
+# driving.py
+#
 import xlwings as xw
 path=r"E:\01_Project\02_groupexcel\4.xlsx"
 
@@ -8,11 +11,11 @@ def determine_group(rng,row,start_column,end_column):
     return True
 
 def auto_group(ws,rng):
-    start_column = rng.column
-    for column in range(rng.last_cell.column,rng.column,-1):
-        start_row = rng.row
-        end_row = rng.row
-        for row in range(rng.row,rng.last_cell.row):
+    start_column = 1
+    for column in range(rng.columns.count,1,-1):
+        start_row = 1
+        end_row = 1
+        for row in range(1,rng.rows.count):
             if determine_group(rng,row,start_column,column) == False:
                 if start_row != end_row:
                     group_range = rng(start_row,column).get_address()+":"+rng(end_row-1,column).get_address()
@@ -24,12 +27,12 @@ def auto_group(ws,rng):
                 end_row += 1
         
         if start_row != end_row:
-            group_range = rng(start_row,column).get_address()+":"+rng(end_row-1,column).get_address()
+            group_range = rng(start_row,column).get_address()+":"+rng(end_row,column).get_address()
             ws.range(group_range).api.Rows.Group()
     return 
 
 wb = xw.Book(path)
 ws = wb.sheets['Sheet1']
-ws.api.Range("A1:E80").ClearOutline
-auto_group(ws,ws.range('A2:E60'))
+ws.api.Range('C4:G62').ClearOutline
+auto_group(ws,ws.range('C4:G62'))
 wb.save()
